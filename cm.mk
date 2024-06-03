44aa414f194821d8a03d9a1b2aec9a2e3ce4df23
Coconaatti <114370773+Coconaatti@users.noreply.github.com>
2024-06-03 09:55:28 +0300

    Update and rename cm.mk.template to cm.mk

---
+++ cm.mk
@@ -0,0 +1,15 @@
+# Release name
+PRODUCT_RELEASE_NAME := YarisL_GSM
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/TCT/YarisL_GSM/device_YarisL_GSM.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := YarisL_GSM
+PRODUCT_NAME := cm_YarisL_GSM
+PRODUCT_BRAND := TCT
+PRODUCT_MODEL := YarisL_GSM
+PRODUCT_MANUFACTURER := TCT
