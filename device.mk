5a994616db5f3a03c853079da428fe92e599f771
Coconaatti <114370773+Coconaatti@users.noreply.github.com>
2024-06-03 10:02:57 +0300

    Update and rename device.mk.template to device.mk

--- device.mk
+++ device.mk
@@ -3,13 +3,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # The gps config appropriate for this device
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
-$(call inherit-product-if-exists, vendor/__MANUFACTURER__/__DEVICE__/__DEVICE__-vendor.mk)
+$(call inherit-product-if-exists, vendor/TCT/YarisL_GSM/YarisL_GSM-vendor.mk)
 
-DEVICE_PACKAGE_OVERLAYS += device/__MANUFACTURER__/__DEVICE__/overlay
+LOCAL_PATH := device/TCT/YarisL_GSM
+
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
 
 
 ifeq ($(TARGET_PREBUILT_KERNEL),)
-	LOCAL_KERNEL := device/__MANUFACTURER__/__DEVICE__/kernel
+	LOCAL_KERNEL := $(LOCAL_PATH)/kernel
 else
 	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
 endif
@@ -21,4 +24,4 @@ $(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := full___DEVICE__
-PRODUCT_DEVICE := __DEVICE__
+PRODUCT_DEVICE := YarisL_GSM
